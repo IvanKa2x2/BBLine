e2ffbc84f47cf9af9e9950f418ce6d0a017696a3
IvanKa567 <i9254484992@gmail.com>
2025-05-25 16:39:06 +0300

    заново

--- tests/test_utils.py
+++
@@ -1,7 +0,0 @@
-from analysis.utils import normalize
-
-
-def test_normalize():
-    assert normalize("A♠", "K♠", True) == "AKs"
-    assert normalize("A♠", "K♣", False) == "AKo"
-    assert normalize("T♦", "T♠", False) == "TT"
